6de53778a589144e09087ec74231988612f9ffea
Tkacz <gabrielmt2@al.insper.edu.br>
2022-02-23 05:47:33 -0300

    att

--- solver.py
+++ solver.py
@@ -4,7 +4,14 @@ def main():
     with open('wordlist.json', 'r') as read_file:
         wordlist = json.load(read_file)
         
-    print(type(wordlist))
+    wordlist = [s for s in wordlist if len(s) < 10]
+        
+    while True:
+        scramble = input('Please input the nine letters provided: ')
+        if len(scramble)==9:
+            break
+        else:
+            print('Invalid number of letters, try again.')
 
 if __name__ == '__main__':
     main()
