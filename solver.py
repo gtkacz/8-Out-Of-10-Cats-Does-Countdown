9529acfa4a631a783cabc30338476a3d1e5fbec1
Tkacz <gabrielmt2@al.insper.edu.br>
2022-02-23 05:58:16 -0300

    att

--- solver.py
+++ solver.py
@@ -4,27 +4,14 @@ def main():
     with open('wordlist.json', 'r') as read_file:
         wordlist = json.load(read_file)
         
-    wordlist_filtered = [s for s in wordlist if len(s) < 10]
+    max_letters = 9
     
-    wordlist_9 = [s for s in wordlist_filtered if len(s) == 9]
-    wordlist_8 = [s for s in wordlist_filtered if len(s) == 8]
-    wordlist_7 = [s for s in wordlist_filtered if len(s) == 7]
-    wordlist_6 = [s for s in wordlist_filtered if len(s) == 6]
-    wordlist_5 = [s for s in wordlist_filtered if len(s) == 5]
-    wordlist_4 = [s for s in wordlist_filtered if len(s) == 4]
-    wordlist_3 = [s for s in wordlist_filtered if len(s) == 3]
-    wordlist_2 = [s for s in wordlist_filtered if len(s) == 2]
+    wordlist_filtered = [s for s in wordlist if len(s) <= max_letters]
     
-    wordlist_dict = {
-        '9':wordlist_9,
-        '8':wordlist_8,
-        '7':wordlist_7,
-        '6':wordlist_6,
-        '5':wordlist_5,
-        '4':wordlist_4,
-        '3':wordlist_3,
-        '2':wordlist_2
-                     }
+    wordlist_dict = {}
+    
+    for i in range(2, max_letters+1):
+        wordlist_dict[i] = [s for s in wordlist_filtered if len(s) == i]
         
     while True:
         scramble = input('Please input the nine letters provided: ')
